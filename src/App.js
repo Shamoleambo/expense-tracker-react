de2e18e8765006e18774ef14be161a838f06bb88
Tiago Gomes <tidgomes@gmail.com>
2021-12-22 10:33:00 -0300

    addition of the general css, import of Header inside App and creation of Header and Balance

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+      </div>
+    </div>
+  );
 }
 
 export default App;
